56459429ba1d8f9f4dcc63336076ef2b6281ab92
Samuel Dalvai <s.dalvai@ewo.com>
2025-02-18 12:57:03 +0100

    feat: update performance test for validating engine speed

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,23 +1,70 @@
+import RigidBodyComponent from '../src/components/RigidBodyComponent';
+import TransformComponent from '../src/components/TransformComponent';
+import Registry from '../src/ecs/Registry';
+import MovementSystem from '../src/systems/MovementSystem';
+
 describe('Testing performance related functions', () => {
     test('A test on performance', () => {
-        const times: number[] = [];
+        const timeAdd: number[] = [];
+        const timeUpdate: number[] = [];
+        const timeDelete: number[] = [];
+        const timesTotal: number[] = [];
 
         for (let x = 0; x < 10; x++) {
             const start = performance.now();
+            const registry = new Registry();
+            registry.addSystem(MovementSystem);
 
-            // Code to be tested
-            for (let i = 0; i < 1000; i++) {
-                //
+            for (let i = 0; i < 10000; i++) {
+                const entity = registry.createEntity();
+                entity.addComponent(RigidBodyComponent, { x: 10, y: 10 });
+                entity.addComponent(TransformComponent, { x: 100, y: 100 });
             }
 
-            const end = performance.now();
-            times.push(end - start);
+            registry.update();
+
+            timeAdd.push(performance.now() - start);
+            const startUpdate = performance.now();
+
+            const system = registry.getSystem(MovementSystem);
+
+            system?.update(10);
+            timeUpdate.push(performance.now() - startUpdate);
+
+            const startDelete = performance.now();
+
+            system?.getSystemEntities().forEach(entity => entity.kill());
+            registry.update();
+
+            timeDelete.push(performance.now() - startDelete);
+
+            timesTotal.push(performance.now() - start);
+
+            expect(system?.getSystemEntities().length).toBe(0);
         }
 
         let sum = 0;
-        for (let x = 0; x < times.length; x++) {
-            sum += times[x];
+        for (let x = 0; x < timeAdd.length; x++) {
+            sum += timeAdd[x];
         }
-        // console.log('Time elapsed: ' + sum / times.length + ' ms');
+        console.log('Time for adding entitites: ' + sum / timeAdd.length + ' ms');
+
+        sum = 0;
+        for (let x = 0; x < timeAdd.length; x++) {
+            sum += timeUpdate[x];
+        }
+        console.log('Time for updating entitites: ' + sum / timeUpdate.length + ' ms');
+
+        sum = 0;
+        for (let x = 0; x < timeDelete.length; x++) {
+            sum += timeDelete[x];
+        }
+        console.log('Time for deleting entitites: ' + sum / timeDelete.length + ' ms');
+
+        sum = 0;
+        for (let x = 0; x < timesTotal.length; x++) {
+            sum += timesTotal[x];
+        }
+        console.log('Total time: ' + sum / timesTotal.length + ' ms');
     });
 });
