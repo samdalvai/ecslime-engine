ea36b6abb0617e50600c34c11ef1e52ca39730b0
Samuel Dalvai <s.dalvai@ewo.com>
2025-02-21 17:25:18 +0100

    feat: remove EntityDestinationComponent from entity when destination is reached and factor in tranform and sprite offset

--- src/systems/EntityDestinationSystem.ts
+++ src/systems/EntityDestinationSystem.ts
@@ -1,14 +1,17 @@
 import EntityDestinationComponent from '../components/EntityDestinationComponent';
 import RigidBodyComponent from '../components/RigidBodyComponent';
+import SpriteComponent from '../components/SpriteComponent';
 import TransformComponent from '../components/TransformComponent';
 import System from '../ecs/System';
 import { Vector } from '../types';
+import RenderEntityDestinationSystem from './RenderEntityDestinationSystem';
 
 export default class EntityDestinationSystem extends System {
     constructor() {
         super();
         this.requireComponent(TransformComponent);
         this.requireComponent(RigidBodyComponent);
+        this.requireComponent(SpriteComponent);
         this.requireComponent(EntityDestinationComponent);
     }
 
@@ -16,15 +19,30 @@ export default class EntityDestinationSystem extends System {
         for (const entity of this.getSystemEntities()) {
             const transform = entity.getComponent(TransformComponent);
             const rigidBody = entity.getComponent(RigidBodyComponent);
+            const sprite = entity.getComponent(SpriteComponent);
             const entityDestination = entity.getComponent(EntityDestinationComponent);
 
-            if (!transform || !rigidBody || !entityDestination) {
+            if (!transform || !sprite || !rigidBody || !entityDestination) {
                 throw new Error('Could not find some component(s) of entity with id ' + entity.getId());
             }
 
+            if (
+                Math.abs(
+                    entityDestination.destinationX - (transform.position.x + (sprite.width / 2) * transform.scale.x),
+                ) <= 10 &&
+                Math.abs(
+                    entityDestination.destinationY - (transform.position.y + (sprite.height / 2) * transform.scale.y),
+                ) <= 10
+            ) {
+                entity.removeComponent(EntityDestinationComponent);
+                entity.removeFromSystem(RenderEntityDestinationSystem);
+                entity.removeFromSystem(EntityDestinationSystem);
+                continue;
+            }
+
             const directionVector = this.computeDirectionVector(
-                transform.position.x,
-                transform.position.y,
+                transform.position.x + (sprite.width / 2) * transform.scale.x,
+                transform.position.y + (sprite.height / 2) * transform.scale.y,
                 entityDestination.destinationX,
                 entityDestination.destinationY,
                 entityDestination.velocity,
