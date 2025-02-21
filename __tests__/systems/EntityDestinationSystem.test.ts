8a6330bb5e35c4935fda9d67ad1764f21d1c916b
Samuel Dalvai <s.dalvai@ewo.com>
2025-02-21 18:11:16 +0100

    chore: remove commented code in EntityDestination system test

--- __tests__/systems/EntityDestinationSystem.test.ts
+++ __tests__/systems/EntityDestinationSystem.test.ts
@@ -6,10 +6,7 @@ import EntityDestinationSystem from '../../src/systems/EntityDestinationSystem';
 describe('Testing EntityDestination system related functions', () => {
     test('Should correctly change entity direction based on velocity', () => {
         const system = new EntityDestinationSystem();
-
         const rigidBody = new RigidBodyComponent();
-        system.updateRigidBodyDirection(0, 0, rigidBody);
-        //expect(rigidBody.direction).toEqual({ x: 0, y: 0 });
 
         /**
          * Case 1
