9bfa4c3634063458d4766a96ce7926d30ac90098
Samuel Dalvai <s.dalvai@ewo.com>
2025-02-22 21:50:22 +0100

    feat: add blueprint for diagonal movement controlled by keyboard

--- src/systems/KeyboardControlSystem.ts
+++ src/systems/KeyboardControlSystem.ts
@@ -5,10 +5,9 @@ import System from '../ecs/System';
 import EventBus from '../event-bus/EventBus';
 import KeyPressedEvent from '../events/KeyPressedEvent';
 import KeyReleasedEvent from '../events/KeyReleasedEvent';
-import { Direction } from '../types';
 
 export default class KeyboardControlSystem extends System {
-    keysPressed: Direction[] = [];
+    keysPressed: string[] = [];
 
     constructor() {
         super();
@@ -23,45 +22,14 @@ export default class KeyboardControlSystem extends System {
     };
 
     onKeyPressed = (event: KeyPressedEvent) => {
-        switch (event.keyCode) {
-            case 'ArrowUp':
-            case 'KeyW':
-                this.keysPressed.push(Direction.UP);
-                break;
-            case 'ArrowRight':
-            case 'KeyD':
-                this.keysPressed.push(Direction.RIGHT);
-                break;
-            case 'ArrowDown':
-            case 'KeyS':
-                this.keysPressed.push(Direction.DOWN);
-                break;
-            case 'ArrowLeft':
-            case 'KeyA':
-                this.keysPressed.push(Direction.LEFT);
-                break;
+        const validKeys = ['ArrowUp', 'KeyW', 'ArrowRight', 'KeyD', 'ArrowDown', 'KeyS', 'ArrowLeft', 'KeyA'];
+        if (validKeys.includes(event.keyCode) && !this.keysPressed.includes(event.keyCode)) {
+            this.keysPressed.push(event.keyCode);
         }
     };
 
     onKeyReleased = (event: KeyReleasedEvent) => {
-        switch (event.keyCode) {
-            case 'ArrowUp':
-            case 'KeyW':
-                this.keysPressed = this.keysPressed.filter(key => key !== Direction.UP);
-                break;
-            case 'ArrowRight':
-            case 'KeyD':
-                this.keysPressed = this.keysPressed.filter(key => key !== Direction.RIGHT);
-                break;
-            case 'ArrowDown':
-            case 'KeyS':
-                this.keysPressed = this.keysPressed.filter(key => key !== Direction.DOWN);
-                break;
-            case 'ArrowLeft':
-            case 'KeyA':
-                this.keysPressed = this.keysPressed.filter(key => key !== Direction.LEFT);
-                break;
-        }
+        this.keysPressed = this.keysPressed.filter(key => key !== event.keyCode);
     };
 
     update = () => {
@@ -73,42 +41,74 @@ export default class KeyboardControlSystem extends System {
                 throw new Error('Could not find some component(s) of entity with id ' + entity.getId());
             }
 
+            console.log(this.keysPressed);
+
             if (this.keysPressed.length == 0) {
                 rigidBody.velocity = { x: 0, y: 0 };
             } else {
-                this.updateEntityMovement(rigidBody, keyboardControl, this.keysPressed[this.keysPressed.length - 1]);
+                console.log('direction: ', this.getMovementDirection(keyboardControl.velocity));
+
+                rigidBody.velocity = this.getMovementDirection(keyboardControl.velocity);
+                //this.updateEntityMovement(rigidBody, keyboardControl, this.keysPressed[this.keysPressed.length - 1]);
             }
         }
     };
 
-    updateEntityMovement = (
-        rigidBody: RigidBodyComponent,
-        keyboardControl: KeyboardControlComponent,
-        movementDirection: Direction,
-    ) => {
-        switch (movementDirection) {
-            case Direction.UP:
-                rigidBody.velocity.x = 0;
-                rigidBody.velocity.y = -1 * keyboardControl.velocity;
-                rigidBody.direction = { x: 0, y: -1 };
-                break;
-            case Direction.RIGHT:
-                rigidBody.velocity.y = 0;
-                rigidBody.velocity.x = keyboardControl.velocity;
-                rigidBody.direction = { x: 1, y: 0 };
-                break;
-            case Direction.DOWN:
-                rigidBody.velocity.x = 0;
-                rigidBody.velocity.y = keyboardControl.velocity;
-                rigidBody.direction = { x: 0, y: 1 };
-                break;
-            case Direction.LEFT:
-                rigidBody.velocity.y = 0;
-                rigidBody.velocity.x = -1 * keyboardControl.velocity;
-                rigidBody.direction = { x: -1, y: 0 };
-                break;
-            default:
-                break;
-        }
+    getMovementDirection = (velocity: number) => {
+        const keyMap: { [key: string]: { x: number; y: number } } = {
+            ArrowUp: { x: 0, y: -velocity },
+            KeyW: { x: 0, y: -velocity },
+            ArrowDown: { x: 0, y: velocity },
+            KeyS: { x: 0, y: velocity },
+            ArrowLeft: { x: -velocity, y: 0 },
+            KeyA: { x: -velocity, y: 0 },
+            ArrowRight: { x: velocity, y: 0 },
+            KeyD: { x: velocity, y: 0 },
+        };
+
+        let x = 0,
+            y = 0;
+
+        const recentKeys = this.keysPressed.slice(-2);
+
+        recentKeys.forEach(key => {
+            if (keyMap[key]) {
+                x += keyMap[key].x;
+                y += keyMap[key].y;
+            }
+        });
+
+        return { x, y };
     };
+
+    // updateEntityMovement = (
+    //     rigidBody: RigidBodyComponent,
+    //     keyboardControl: KeyboardControlComponent,
+    //     movementDirection: Direction,
+    // ) => {
+    //     switch (movementDirection) {
+    //         case Direction.UP:
+    //             rigidBody.velocity.x = 0;
+    //             rigidBody.velocity.y = -1 * keyboardControl.velocity;
+    //             rigidBody.direction = { x: 0, y: -1 };
+    //             break;
+    //         case Direction.RIGHT:
+    //             rigidBody.velocity.y = 0;
+    //             rigidBody.velocity.x = keyboardControl.velocity;
+    //             rigidBody.direction = { x: 1, y: 0 };
+    //             break;
+    //         case Direction.DOWN:
+    //             rigidBody.velocity.x = 0;
+    //             rigidBody.velocity.y = keyboardControl.velocity;
+    //             rigidBody.direction = { x: 0, y: 1 };
+    //             break;
+    //         case Direction.LEFT:
+    //             rigidBody.velocity.y = 0;
+    //             rigidBody.velocity.x = -1 * keyboardControl.velocity;
+    //             rigidBody.direction = { x: -1, y: 0 };
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // };
 }
