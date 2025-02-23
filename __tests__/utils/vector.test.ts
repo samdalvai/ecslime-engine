11b675fad6424de3cd2f3992c2c1348a385ad8da
Samuel Dalvai <s.dalvai@ewo.com>
2025-02-23 15:31:02 +0100

    test: add unit tests on computeDirectionVector method

--- __tests__/utils/vector.test.ts
+++ __tests__/utils/vector.test.ts
@@ -1,8 +1,34 @@
 import { expect } from '@jest/globals';
 
-import { computeUnitVector } from '../../src/utils/vector';
+import { computeDirectionVector, computeUnitVector } from '../../src/utils/vector';
 
 describe('Testing vector utils related functions', () => {
+    test('Should correctly compute direction vector normalized to length on vertical axis', () => {
+        let directionVector = computeDirectionVector(100, 100, 100, 200, 10);
+        expect(directionVector).toEqual({ x: 0, y: 10 });
+
+        directionVector = computeDirectionVector(100, 100, 100, 0, 10);
+        expect(directionVector).toEqual({ x: 0, y: -10 });
+    });
+
+    test('Should correctly compute direction vector normalized to length on horizontal axis', () => {
+        let directionVector = computeDirectionVector(100, 100, 200, 100, 10);
+        expect(directionVector).toEqual({ x: 10, y: 0 });
+
+        directionVector = computeDirectionVector(100, 100, 0, 100, 10);
+        expect(directionVector).toEqual({ x: -10, y: 0 });
+    });
+
+    test('Should correctly compute direction vector normalized to length on diagonal axis', () => {
+        let directionVector = computeDirectionVector(100, 100, 200, 200, 10);
+        expect(directionVector.x - 7).toBeLessThan(0.1);
+        expect(directionVector.y - 7).toBeLessThan(0.1);
+
+        directionVector = computeDirectionVector(200, 200, 100, 100, 10);
+        expect(directionVector.x + 7).toBeLessThan(0.1);
+        expect(directionVector.y + 7).toBeLessThan(0.1);
+    });
+
     test('Should correctly compute unit vector based on x and y', () => {
         /**
          * Case 1
