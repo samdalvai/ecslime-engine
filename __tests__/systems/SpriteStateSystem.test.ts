8eb1a13b355e37793b53c596a670a272a1c16ae0
Samuel Dalvai <s.dalvai@ewo.com>
2025-02-23 16:50:05 +0100

    test: add unit tests on SpriteState system for hurt sprite

--- __tests__/systems/SpriteStateSystem.test.ts
+++ __tests__/systems/SpriteStateSystem.test.ts
@@ -70,4 +70,36 @@ describe('Testing SpriteState system related functions', () => {
         system.updateSpriteState(sprite, rigidBody, false);
         expect(sprite.srcRect).toEqual({ x: 0, y: 224, width: 32, height: 32 });
     });
+
+    test('Hurt entity facing up should show moving sprite facing up (1st row)', () => {
+        const sprite = new SpriteComponent('test', 32, 32, 0, 0, 0);
+        const rigidBody = new RigidBodyComponent({ x: 0, y: -100 }, { x: 0, y: -1 });
+
+        system.updateSpriteState(sprite, rigidBody, true);
+        expect(sprite.srcRect).toEqual({ x: 0, y: 256, width: 32, height: 32 });
+    });
+
+    test('Hurt entity facing right should show moving sprite facing right (2nd row)', () => {
+        const sprite = new SpriteComponent('test', 32, 32, 0, 0, 0);
+        const rigidBody = new RigidBodyComponent({ x: 100, y: 0 }, { x: 1, y: 0 });
+
+        system.updateSpriteState(sprite, rigidBody, true);
+        expect(sprite.srcRect).toEqual({ x: 0, y: 288, width: 32, height: 32 });
+    });
+
+    test('Hurt entity facing down should show moving sprite facing down (3rd row)', () => {
+        const sprite = new SpriteComponent('test', 32, 32, 0, 0, 0);
+        const rigidBody = new RigidBodyComponent({ x: 0, y: 100 }, { x: 0, y: 1 });
+
+        system.updateSpriteState(sprite, rigidBody, true);
+        expect(sprite.srcRect).toEqual({ x: 0, y: 320, width: 32, height: 32 });
+    });
+
+    test('Hurt entity facing left should show moving sprite facing left (4th row)', () => {
+        const sprite = new SpriteComponent('test', 32, 32, 0, 0, 0);
+        const rigidBody = new RigidBodyComponent({ x: -100, y: 0 }, { x: -1, y: 0 });
+
+        system.updateSpriteState(sprite, rigidBody, true);
+        expect(sprite.srcRect).toEqual({ x: 0, y: 352, width: 32, height: 32 });
+    });
 });
