197ccb00982dd48f237d4ef4b0feef09d155d375
Samuel Dalvai <s.dalvai@ewo.com>
2025-05-21 13:40:35 +0200

    feat: implement deserialization for all component types

--- src/__tests__/deserialization/deserialization.test.ts
+++ src/__tests__/deserialization/deserialization.test.ts
@@ -1,5 +1,6 @@
 import { expect } from '@jest/globals';
 
+import RigidBodyComponent from '../../components/RigidBodyComponent';
 import TransformComponent from '../../components/TransformComponent';
 import Registry from '../../ecs/Registry';
 import { deserializeEntity } from '../../serialization/deserialization';
@@ -35,6 +36,56 @@ describe('Testing deserialization related functions', () => {
                 y: 1,
             },
             rotation: 0,
+        });
+    });
+
+    test('Should deserialize entity Map to Entity with two components', () => {
+        const registry = new Registry();
+
+        const entityMap: EntityMap = {
+            components: [
+                {
+                    name: 'transform',
+                    properties: {
+                        position: { x: 100, y: 100 },
+                        scale: { x: 1, y: 1 },
+                        rotation: 0,
+                    },
+                },
+                {
+                    name: 'rigidbody',
+                    properties: {
+                        velocity: { x: 100, y: 100 },
+                        direction: { x: 1, y: 0 },
+                    },
+                },
+            ],
+        };
+
+        const entity = deserializeEntity(entityMap, registry);
+        const transform = entity.getComponent(TransformComponent);
+        const rigidbody = entity.getComponent(RigidBodyComponent);
+
+        expect(transform).toEqual({
+            position: {
+                x: 100,
+                y: 100,
+            },
+            scale: {
+                x: 1,
+                y: 1,
+            },
+            rotation: 0,
+        });
+        expect(rigidbody).toEqual({
+            velocity: {
+                x: 100,
+                y: 100,
+            },
+            direction: {
+                x: 1,
+                y: 0,
+            },
         });
     });
 });
--- src/serialization/deserialization.ts
+++ src/serialization/deserialization.ts
@@ -1,3 +1,30 @@
+import AnimationComponent from '../components/AnimationComponent';
+import BoxColliderComponent from '../components/BoxColliderComponent';
+import CameraFollowComponent from '../components/CameraFollowComponent';
+import CameraShakeComponent from '../components/CameraShakeComponent';
+import DamageRadiusComponent from '../components/DamageRadiusComponent';
+import DeadBodyOnDeathComponent from '../components/DeadBodyOnDeathComponent';
+import EntityDestinationComponent from '../components/EntityDestinationComponent';
+import EntityEffectComponent from '../components/EntityEffectComponent';
+import EntityFollowComponent from '../components/EntityFollowComponent';
+import HealthComponent from '../components/HealthComponent';
+import HighlightComponent from '../components/HighlightComponent';
+import LifetimeComponent from '../components/LifetimeComponent';
+import LightEmitComponent from '../components/LightEmitComponent';
+import ParticleComponent from '../components/ParticleComponent';
+import ParticleEmitComponent from '../components/ParticleEmitComponent';
+import PlayerControlComponent from '../components/PlayerControlComponent';
+import ProjectileComponent from '../components/ProjectileComponent';
+import RangedAttackEmitterComponent from '../components/RangedAttackEmitterComponent';
+import RigidBodyComponent from '../components/RigidBodyComponent';
+import ScriptComponent from '../components/ScriptComponent';
+import ShadowComponent from '../components/ShadowComponent';
+import SlowTimeComponent from '../components/SlowTimeComponent';
+import SoundComponent from '../components/SoundComponent';
+import SpriteComponent from '../components/SpriteComponent';
+import SpriteStateComponent from '../components/SpriteStateComponent';
+import TeleportComponent from '../components/TeleportComponent';
+import TextLabelComponent from '../components/TextLabelComponent';
 import TransformComponent from '../components/TransformComponent';
 import Entity from '../ecs/Entity';
 import Registry from '../ecs/Registry';
@@ -8,60 +35,238 @@ export const deserializeEntity = (entityMap: EntityMap, registry: Registry): Ent
 
     for (const component of entityMap.components) {
         switch (component.name) {
-            case 'animation':
+            case 'animation': {
+                entity.addComponent(
+                    AnimationComponent,
+                    component.properties.numFrames,
+                    component.properties.currentFrame,
+                    component.properties.frameSpeedRate,
+                    component.properties.isLoop,
+                    component.properties.startTime,
+                );
                 break;
-            case 'boxcollider':
+            }
+            case 'boxcollider': {
+                entity.addComponent(
+                    BoxColliderComponent,
+                    component.properties.width,
+                    component.properties.height,
+                    component.properties.offset,
+                );
                 break;
-            case 'camerafollow':
+            }
+            case 'camerafollow': {
+                entity.addComponent(CameraFollowComponent);
                 break;
-            case 'camerashake':
+            }
+            case 'camerashake': {
+                entity.addComponent(CameraShakeComponent, component.properties.shakeDuration);
                 break;
-            case 'damageradius':
+            }
+            case 'damageradius': {
+                entity.addComponent(
+                    DamageRadiusComponent,
+                    component.properties.radius,
+                    component.properties.damagePerSecond,
+                    component.properties.isFriendly,
+                );
                 break;
-            case 'deadbodyondeath':
+            }
+            case 'deadbodyondeath': {
+                entity.addComponent(DeadBodyOnDeathComponent);
                 break;
-            case 'entitydestination':
+            }
+            case 'entitydestination': {
+                entity.addComponent(
+                    EntityDestinationComponent,
+                    component.properties.destinationX,
+                    component.properties.destinationY,
+                    component.properties.velocity,
+                );
                 break;
-            case 'entityeffect':
+            }
+            case 'entityeffect': {
+                entity.addComponent(
+                    EntityEffectComponent,
+                    component.properties.slowed,
+                    component.properties.slowedPercentage,
+                    component.properties.hasDamageOverTime,
+                    component.properties.damagePerSecond,
+                    component.properties.lastDamageTime,
+                );
                 break;
-            case 'entityfollow':
+            }
+            case 'entityfollow': {
+                entity.addComponent(
+                    EntityFollowComponent,
+                    component.properties.detectionRadius,
+                    component.properties.minFollowDistance,
+                    component.properties.followVelocity,
+                    component.properties.followDuration,
+                    component.properties.followedEntity,
+                    component.properties.startFollowTime,
+                );
                 break;
-            case 'health':
+            }
+            case 'health': {
+                entity.addComponent(
+                    HealthComponent,
+                    component.properties.healthPercentage,
+                    component.properties.lastDamageTime,
+                );
                 break;
-            case 'highlight':
+            }
+            case 'highlight': {
+                entity.addComponent(
+                    HighlightComponent,
+                    component.properties.isHighlighted,
+                    component.properties.width,
+                    component.properties.height,
+                    component.properties.offsetX,
+                    component.properties.offsetY,
+                );
                 break;
-            case 'lifetime':
+            }
+            case 'lifetime': {
+                entity.addComponent(LifetimeComponent, component.properties.lifetime, component.properties.startTime);
                 break;
-            case 'lightemit':
+            }
+            case 'lightemit': {
+                entity.addComponent(LightEmitComponent, component.properties.lightRadius);
                 break;
-            case 'particle':
+            }
+            case 'particle': {
+                entity.addComponent(ParticleComponent, component.properties.dimension, component.properties.color);
                 break;
-            case 'particleemit':
+            }
+            case 'particleemit': {
+                entity.addComponent(
+                    ParticleEmitComponent,
+                    component.properties.dimension,
+                    component.properties.duration,
+                    component.properties.color,
+                    component.properties.emitFrequency,
+                    component.properties.emitRadius,
+                    component.properties.offsetX,
+                    component.properties.offsetY,
+                    component.properties.particleVelocity,
+                    component.properties.lastEmission,
+                );
                 break;
-            case 'playercontrol':
+            }
+            case 'playercontrol': {
+                entity.addComponent(
+                    PlayerControlComponent,
+                    component.properties.velocity,
+                    component.properties.keysPressed,
+                    component.properties.magicBubbleCooldown,
+                    component.properties.magicBubbleLastEmissionTime,
+                    component.properties.teleportCooldown,
+                    component.properties.teleportLastEmissionTime,
+                    component.properties.fireCircleCooldown,
+                    component.properties.fireCircleLastEmissionTime,
+                );
                 break;
-            case 'projectile':
+            }
+            case 'projectile': {
+                entity.addComponent(
+                    ProjectileComponent,
+                    component.properties.isFriendly,
+                    component.properties.hitPercentDamage,
+                );
                 break;
-            case 'rangedattackemitter':
+            }
+            case 'rangedattackemitter': {
+                entity.addComponent(
+                    RangedAttackEmitterComponent,
+                    component.properties.projectileVelocity,
+                    component.properties.repeatFrequency,
+                    component.properties.projectileDuration,
+                    component.properties.hitPercentDamage,
+                    component.properties.isFriendly,
+                    component.properties.lastEmissionTime,
+                );
                 break;
-            case 'rigidbody':
+            }
+            case 'rigidbody': {
+                entity.addComponent(RigidBodyComponent, component.properties.velocity, component.properties.direction);
                 break;
-            case 'script':
+            }
+            case 'script': {
+                entity.addComponent(
+                    ScriptComponent,
+                    component.properties.scripts,
+                    component.properties.currentActionIndex,
+                    component.properties.actionStart,
+                );
                 break;
-            case 'shadow':
+            }
+            case 'shadow': {
+                entity.addComponent(
+                    ShadowComponent,
+                    component.properties.width,
+                    component.properties.height,
+                    component.properties.offsetX,
+                    component.properties.offsetY,
+                );
                 break;
-            case 'slowtime':
+            }
+            case 'slowtime': {
+                entity.addComponent(
+                    SlowTimeComponent,
+                    component.properties.radius,
+                    component.properties.slowTimePercentage,
+                    component.properties.isFriendly,
+                );
                 break;
-            case 'sound':
+            }
+            case 'sound': {
+                entity.addComponent(
+                    SoundComponent,
+                    component.properties.assetId,
+                    component.properties.offsetBuffer,
+                    component.properties.volume,
+                );
                 break;
-            case 'sprite':
+            }
+            case 'sprite': {
+                entity.addComponent(
+                    SpriteComponent,
+                    component.properties.assetId,
+                    component.properties.width,
+                    component.properties.height,
+                    component.properties.zIndex,
+                    component.properties.srcRect.x,
+                    component.properties.srcRect.y,
+                    component.properties.flip,
+                    component.properties.isFixed,
+                    component.properties.transparency,
+                );
                 break;
-            case 'spritestate':
+            }
+            case 'spritestate': {
+                entity.addComponent(SpriteStateComponent);
                 break;
-            case 'teleport':
+            }
+            case 'teleport': {
+                entity.addComponent(
+                    TeleportComponent,
+                    component.properties.isTeleporting,
+                    component.properties.teleportDelay,
+                );
                 break;
-            case 'textlabel':
+            }
+            case 'textlabel': {
+                entity.addComponent(
+                    TextLabelComponent,
+                    component.properties.position,
+                    component.properties.text,
+                    component.properties.color,
+                    component.properties.isFixed,
+                    component.properties.font,
+                );
                 break;
+            }
             case 'transform': {
                 entity.addComponent(
                     TransformComponent,
