76e06766c6326e15e140589c915c2f790015b06d
Samuel Dalvai <s.dalvai@ewo.com>
2025-05-21 14:25:01 +0200

    feat: implement more serialization of entities

--- src/serialization/serialization.ts
+++ src/serialization/serialization.ts
@@ -1,4 +1,8 @@
 import AnimationComponent from '../components/AnimationComponent';
+import BoxColliderComponent from '../components/BoxColliderComponent';
+import CameraFollowComponent from '../components/CameraFollowComponent';
+import CameraShakeComponent from '../components/CameraShakeComponent';
+import DamageRadiusComponent from '../components/DamageRadiusComponent';
 import RigidBodyComponent from '../components/RigidBodyComponent';
 import TransformComponent from '../components/TransformComponent';
 import Entity from '../ecs/Entity';
@@ -13,7 +17,7 @@ export const serializeEntity = (entity: Entity): EntityMap => {
         const animation = entity.getComponent(AnimationComponent);
 
         if (!animation) {
-            throw new Error('Could not find some animation component of entity with id ' + entity.getId());
+            throw new Error('Could not find animation component of entity with id ' + entity.getId());
         }
 
         components.push({
@@ -27,10 +31,72 @@ export const serializeEntity = (entity: Entity): EntityMap => {
             },
         });
     }
+
     // case 'boxcollider'
+    if (entity.hasComponent(BoxColliderComponent)) {
+        const collider = entity.getComponent(BoxColliderComponent);
+
+        if (!collider) {
+            throw new Error('Could not find collider component of entity with id ' + entity.getId());
+        }
+
+        components.push({
+            name: 'boxcollider',
+            properties: {
+                width: collider.width,
+                height: collider.height,
+                offset: collider.offset,
+            },
+        });
+    }
+
     // case 'camerafollow'
+    if (entity.hasComponent(CameraFollowComponent)) {
+        const cameraFollow = entity.getComponent(CameraFollowComponent);
+
+        if (!cameraFollow) {
+            throw new Error('Could not find camerafollow component of entity with id ' + entity.getId());
+        }
+
+        components.push({
+            name: 'camerafollow',
+            properties: {},
+        });
+    }
+
     // case 'camerashake'
+    if (entity.hasComponent(CameraShakeComponent)) {
+        const cameraShake = entity.getComponent(CameraShakeComponent);
+
+        if (!cameraShake) {
+            throw new Error('Could not find camerashake component of entity with id ' + entity.getId());
+        }
+
+        components.push({
+            name: 'camerashake',
+            properties: {
+                shakeDuration: cameraShake.shakeDuration,
+            },
+        });
+    }
+
     // case 'damageradius'
+    if (entity.hasComponent(DamageRadiusComponent)) {
+        const damageradius = entity.getComponent(DamageRadiusComponent);
+
+        if (!damageradius) {
+            throw new Error('Could not find damageradius component of entity with id ' + entity.getId());
+        }
+
+        components.push({
+            name: 'damageradius',
+            properties: {
+                radius: damageradius.radius,
+                damagePerSecond: damageradius.damagePerSecond,
+                isFriendly: damageradius.isFriendly,
+            },
+        });
+    }
     // case 'deadbodyondeath'
     // case 'entitydestination'
     // case 'entityeffect'
@@ -44,12 +110,13 @@ export const serializeEntity = (entity: Entity): EntityMap => {
     // case 'playercontrol'
     // case 'projectile'
     // case 'rangedattackemitter'
+
     // case 'rigidbody'
     if (entity.hasComponent(RigidBodyComponent)) {
         const rigidbody = entity.getComponent(RigidBodyComponent);
 
         if (!rigidbody) {
-            throw new Error('Could not find some rigidbody component of entity with id ' + entity.getId());
+            throw new Error('Could not find rigidbody component of entity with id ' + entity.getId());
         }
 
         components.push({
@@ -60,6 +127,7 @@ export const serializeEntity = (entity: Entity): EntityMap => {
             },
         });
     }
+
     // case 'script'
     // case 'shadow'
     // case 'slowtime'
@@ -68,12 +136,13 @@ export const serializeEntity = (entity: Entity): EntityMap => {
     // case 'spritestate'
     // case 'teleport'
     // case 'textlabel'
+
     // case 'transform'
     if (entity.hasComponent(TransformComponent)) {
         const transform = entity.getComponent(TransformComponent);
 
         if (!transform) {
-            throw new Error('Could not find some transform component of entity with id ' + entity.getId());
+            throw new Error('Could not find transform component of entity with id ' + entity.getId());
         }
 
         components.push({
