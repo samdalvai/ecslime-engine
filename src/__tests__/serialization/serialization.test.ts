0dac013de20273b0d8af334f633738e4f515c858
Samuel Dalvai <s.dalvai@ewo.com>
2025-05-21 13:48:35 +0200

    feat: add rigidbody and animation to components serialization

--- src/__tests__/serialization/serialization.test.ts
+++ src/__tests__/serialization/serialization.test.ts
@@ -1,5 +1,6 @@
 import { expect } from '@jest/globals';
 
+import RigidBodyComponent from '../../components/RigidBodyComponent';
 import TransformComponent from '../../components/TransformComponent';
 import Registry from '../../ecs/Registry';
 import { serializeEntity } from '../../serialization/serialization';
@@ -13,6 +14,35 @@ describe('Testing serialization related functions', () => {
 
         const expected: EntityMap = {
             components: [
+                {
+                    name: 'transform',
+                    properties: {
+                        position: { x: 100, y: 100 },
+                        scale: { x: 1, y: 1 },
+                        rotation: 0,
+                    },
+                },
+            ],
+        };
+
+        expect(serializeEntity(entity)).toEqual(expected);
+    });
+
+    test('Should serialize entity with two components to a valid Entity Map', () => {
+        const registry = new Registry();
+        const entity = registry.createEntity();
+        entity.addComponent(TransformComponent, { x: 100, y: 100 }, { x: 1, y: 1 }, 0);
+        entity.addComponent(RigidBodyComponent, { x: 100, y: 100 }, { x: 1, y: 0 });
+
+        const expected: EntityMap = {
+            components: [
+                {
+                    name: 'rigidbody',
+                    properties: {
+                        velocity: { x: 100, y: 100 },
+                        direction: { x: 1, y: 0 },
+                    },
+                },
                 {
                     name: 'transform',
                     properties: {
--- src/serialization/serialization.ts
+++ src/serialization/serialization.ts
@@ -1,3 +1,5 @@
+import AnimationComponent from '../components/AnimationComponent';
+import RigidBodyComponent from '../components/RigidBodyComponent';
 import TransformComponent from '../components/TransformComponent';
 import Entity from '../ecs/Entity';
 import { ComponentType } from '../types/components';
@@ -6,6 +8,67 @@ import { EntityMap } from '../types/map';
 export const serializeEntity = (entity: Entity): EntityMap => {
     const components: ComponentType[] = [];
 
+    // case 'animation'
+    if (entity.hasComponent(AnimationComponent)) {
+        const animation = entity.getComponent(AnimationComponent);
+
+        if (!animation) {
+            throw new Error('Could not find some animation component of entity with id ' + entity.getId());
+        }
+
+        components.push({
+            name: 'animation',
+            properties: {
+                numFrames: animation.numFrames,
+                currentFrame: animation.currentFrame,
+                frameSpeedRate: animation.frameSpeedRate,
+                isLoop: animation.isLoop,
+                startTime: animation.startTime,
+            },
+        });
+    }
+    // case 'boxcollider'
+    // case 'camerafollow'
+    // case 'camerashake'
+    // case 'damageradius'
+    // case 'deadbodyondeath'
+    // case 'entitydestination'
+    // case 'entityeffect'
+    // case 'entityfollow'
+    // case 'health'
+    // case 'highlight'
+    // case 'lifetime'
+    // case 'lightemit'
+    // case 'particle'
+    // case 'particleemit'
+    // case 'playercontrol'
+    // case 'projectile'
+    // case 'rangedattackemitter'
+    // case 'rigidbody'
+    if (entity.hasComponent(RigidBodyComponent)) {
+        const rigidbody = entity.getComponent(RigidBodyComponent);
+
+        if (!rigidbody) {
+            throw new Error('Could not find some rigidbody component of entity with id ' + entity.getId());
+        }
+
+        components.push({
+            name: 'rigidbody',
+            properties: {
+                velocity: rigidbody.velocity,
+                direction: rigidbody.direction,
+            },
+        });
+    }
+    // case 'script'
+    // case 'shadow'
+    // case 'slowtime'
+    // case 'sound'
+    // case 'sprite'
+    // case 'spritestate'
+    // case 'teleport'
+    // case 'textlabel'
+    // case 'transform'
     if (entity.hasComponent(TransformComponent)) {
         const transform = entity.getComponent(TransformComponent);
 
