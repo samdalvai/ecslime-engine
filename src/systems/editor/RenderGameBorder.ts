756cf6afe1b3cdd2b520aa4d0ebe371cf70c2944
Samuel Dalvai <s.dalvai@ewo.com>
2025-06-18 17:29:27 +0200

    feat: add text describing game border

--- src/systems/editor/RenderGameBorder.ts
+++ src/systems/editor/RenderGameBorder.ts
@@ -8,6 +8,10 @@ export default class RenderGameBorder extends System {
     }
 
     update(ctx: CanvasRenderingContext2D, camera: Rectangle, zoom: number) {
+        ctx.fillStyle = 'red';
+        ctx.font = '18px Arial';
+        ctx.fillText('Game border', 0 - camera.x * zoom, 0 - camera.y * zoom - 10);
+
         ctx.strokeStyle = 'red';
         ctx.lineWidth = 2;
         ctx.strokeRect(0 - camera.x * zoom, 0 - camera.y * zoom, Game.mapWidth * zoom, Game.mapHeight * zoom);
