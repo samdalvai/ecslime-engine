70e068506991d8c3a0a2cc95c92fe75d69a6e14b
Samuel Dalvai <s.dalvai@ewo.com>
2025-07-15 08:57:31 +0200

    refactor: handle input creation only if subinputs are not of type object

--- src/editor/systems/RenderSidebarSystem.ts
+++ src/editor/systems/RenderSidebarSystem.ts
@@ -454,24 +454,29 @@ export default class RenderSidebarSystem extends System {
                 const objectContainer = document.createElement('div');
 
                 for (const property in propertyValue) {
-                    if (component.constructor.name === 'ScriptComponent') {
-                        console.log("property: ", property);
-                        console.log("propertyValue: ", propertyValue);
-                        console.log("propertyValue[property]: ", propertyValue[property as keyof typeof propertyValue]);
+                    if (typeof propertyValue[property as keyof typeof propertyValue] !== 'object') {
+                        const textInput = this.createInput(
+                            'number',
+                            propertyName + '-' + property + '-' + entityId,
+                            propertyValue[property as keyof typeof propertyValue],
+                        );
+
+                        textInput.addEventListener('input', event => {
+                            const target = event.target as HTMLInputElement;
+                            propertyValue[property as keyof typeof propertyValue] = parseFloat(target.value);
+                        });
+                        const propertyLi = this.createListItem(propertyName + ' (' + property + ')', textInput);
+                        objectContainer.append(propertyLi);
+                    } else {
+                        if (component.constructor.name === 'ScriptComponent') {
+                            console.log('property: ', property);
+                            console.log('propertyValue: ', propertyValue);
+                            console.log(
+                                'propertyValue[property]: ',
+                                propertyValue[property as keyof typeof propertyValue],
+                            );
+                        }
                     }
-
-                    const textInput = this.createInput(
-                        'number',
-                        propertyName + '-' + property + '-' + entityId,
-                        propertyValue[property as keyof typeof propertyValue],
-                    );
-
-                    textInput.addEventListener('input', event => {
-                        const target = event.target as HTMLInputElement;
-                        propertyValue[property as keyof typeof propertyValue] = parseFloat(target.value);
-                    });
-                    const propertyLi = this.createListItem(propertyName + ' (' + property + ')', textInput);
-                    objectContainer.append(propertyLi);
                 }
 
                 return objectContainer;
