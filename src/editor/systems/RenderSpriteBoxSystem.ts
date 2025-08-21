9707636ec33c8036aeeaba429a12df1d2f8e80be
Samuel Dalvai <s.dalvai@ewo.com>
2025-08-21 16:49:27 +0200

    refactor: render invisible entities having just a transform

--- src/editor/systems/RenderSpriteBoxSystem.ts
+++ src/editor/systems/RenderSpriteBoxSystem.ts
@@ -18,6 +18,7 @@ export default class RenderSpriteBoxSystem extends System {
             entityId: number;
             sprite: SpriteComponent;
             transform: TransformComponent;
+            hasSprite: boolean;
         }[] = [];
 
         for (const entity of this.getSystemEntities()) {
@@ -33,12 +34,12 @@ export default class RenderSpriteBoxSystem extends System {
                     throw new Error('Could not find some component(s) of entity with id ' + entity.getId());
                 }
 
-                renderableEntities.push({ entityId: entity.getId(), sprite, transform });
+                renderableEntities.push({ entityId: entity.getId(), sprite, transform, hasSprite: true });
                 continue;
             }
 
             const mockSprite = new SpriteComponent(DEFAULT_SPRITE, 32, 32, 0);
-            renderableEntities.push({ entityId: entity.getId(), sprite: mockSprite, transform });
+            renderableEntities.push({ entityId: entity.getId(), sprite: mockSprite, transform, hasSprite: false });
         }
 
         renderableEntities.sort((entityA, entityB) => {
@@ -82,6 +83,7 @@ export default class RenderSpriteBoxSystem extends System {
                         ctx.lineWidth = 4;
                         ctx.strokeRect(spriteRect.x, spriteRect.y, spriteRect.width, spriteRect.height);
                         ctx.restore();
+                        spriteBoxHighlighted = true;
                     }
                 }
             }
@@ -128,6 +130,14 @@ export default class RenderSpriteBoxSystem extends System {
                     ctx.restore();
                     spriteBoxHighlighted = true;
                 }
+            }
+
+            if (!spriteBoxHighlighted && !renderableEntities[i].hasSprite) {
+                ctx.save();
+                ctx.strokeStyle = 'gray';
+                ctx.lineWidth = 2;
+                ctx.strokeRect(spriteRect.x, spriteRect.y, spriteRect.width, spriteRect.height);
+                ctx.restore();
             }
         }
     }
