a449f9060714e5f9afcfdc2caaa8614cb0de4ed6
samdalvai <samuel.dalvai@gmail.com>
2025-09-17 08:13:36 +0200

    docs: add TODO comment on editor level testing

--- src/editor/Editor.ts
+++ src/editor/Editor.ts
@@ -27,6 +27,7 @@ declare global {
     }
 }
 
+// TODO: add logic to test level immediately without needing to export level to game
 export default class Editor extends Engine {
     // Object for Editor
     private versionManager: VersionManager;
